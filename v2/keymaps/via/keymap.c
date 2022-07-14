3728b33ff531ee57d4352a1303eb878fd5e0d1f5
Vino Rodrigues <github@m.vinorodrigues.com>
2022-07-15 05:19:05 +1000

    add legacy custom keys and begin wip on iso version

--- v2/keymaps/via/keymap.c
+++ v2/keymaps/via/keymap.c
@@ -16,19 +16,40 @@ enum custom_keycodes {
     #if VIA_ENABLE
     KC_MISSION_CONTROL = USER00,
     KC_LAUNCHPAD,
+    KC_LEFT_OPTION,
+    KC_RIGHT_OPTION,
+    KC_LEFT_COMMAND,
+    KC_RIGHT_COMMAND,
+    KC_TASK_VIEW,
+    KC_FILE_EXPLORER,
     KC_VERSION = USER09
     #else
     KC_MISSION_CONTROL = SAFE_RANGE,
     KC_LAUNCHPAD,
+    KC_LEFT_OPTION,
+    KC_RIGHT_OPTION,
+    KC_LEFT_COMMAND,
+    KC_RIGHT_COMMAND,
+    KC_TASK_VIEW,
+    KC_FILE_EXPLORER,
     KC_VERSION
     #endif
 };
 
-#define KC_MCTL KC_MISSION_CONTROL
-#define KC_LPAD KC_LAUNCHPAD
+enum macos_consumer_usages {
+    _AC_SHOW_ALL_WINDOWS = 0x29F,  // mapped to KC_MCON
+    _AC_SHOW_ALL_APPS    = 0x2A0   // mapped to KC_LPAD
+};
+
+#define MC_MCON KC_MISSION_CONTROL
+#define MC_LPAD KC_LAUNCHPAD
+#define MC_LOPT KC_LEFT_OPTION
+#define MC_ROPT KC_RIGHT_OPTION
+#define MC_LCMD KC_LEFT_COMMAND
+#define MC_RCMD KC_RIGHT_COMMAND
+#define WC_TASK KC_TASK_VIEW
+#define WC_FLXP KC_FILE_EXPLORER
 #define KC_VRSN KC_VERSION
-#define KC_TASK G(KC_TAB)
-#define KC_FLXP G(KC_E)
 #define RGB_RMD RGB_RMOD
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -47,15 +68,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Ctrl│Opt │Cmd │                        │Cmd│Fn1│Ctl││ ← │ ↓ │ → │
      * └────┴────┴────┴────────────────────────┴───┴───┴───┘└───┴───┴───┘
      */
-    [_M0] = LAYOUT_all(
-        KC_ESC,  KC_BRID, KC_BRIU, KC_MCTL, KC_LPAD, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_DEL,           KC_MUTE, KC_VOLD, KC_VOLU,
+    [_M0] = LAYOUT_ansi(
+        KC_ESC,  KC_BRID, KC_BRIU, MC_MCON, MC_LPAD, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_DEL,           KC_MUTE, KC_VOLD, KC_VOLU,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_PGUP,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGDN,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_HOME,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,
-        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, MO(_MF), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, MC_LOPT, MC_LCMD,                            KC_SPC,                             MC_RCMD, MO(_MF), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
-    [_MF] = LAYOUT_all(
+    [_MF] = LAYOUT_ansi(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,          RGB_TOG, RGB_VAD, RGB_VAI,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         RGB_TOG, RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, NK_TOGG, _______, _______, _______, _______, _______, _______, _______,          _______,
@@ -63,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,          QK_BOOT, _______, _______, KC_VRSN, _______, NK_TOGG, _______, _______, _______, EEP_RST,          _______, _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______),
 
-    [_W2] = LAYOUT_all(
+    [_W2] = LAYOUT_ansi(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_MUTE, KC_VOLD, KC_VOLU,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_PGUP,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGDN,
@@ -71,8 +92,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(_WF), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
-    [_WF] = LAYOUT_all(
-        _______, KC_BRID, KC_BRIU, KC_TASK, KC_FLXP, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, _______,          RGB_TOG, RGB_VAD, RGB_VAI,
+    [_WF] = LAYOUT_ansi(
+        _______, KC_BRID, KC_BRIU, WC_TASK, WC_FLXP, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, _______,          RGB_TOG, RGB_VAD, RGB_VAI,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         RGB_TOG, RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, NK_TOGG, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, RGB_RMD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______, _______,          _______,          _______,
@@ -136,24 +157,49 @@ bool dip_switch_update_user(uint8_t index, bool active) {
 
 /* ----- Custom keycodes ----- */
 
+bool __consumer_send(keyrecord_t *record, uint16_t data) {
+    if (record->event.pressed) {
+        host_consumer_send(data);
+    } else {
+        host_consumer_send(0);
+    }
+    return false;  // Skip all further processing of this key
+}
+
+bool __code_1(keyrecord_t *record, uint16_t data) {
+    if (record->event.pressed) {
+        register_code(data);
+    } else {
+        unregister_code(data);
+    }
+    return false;  // Skip all further processing of this key
+}
+
+bool __code_2(keyrecord_t *record, uint16_t data1, uint16_t data2) {
+    if (record->event.pressed) {
+        register_code(data1);
+        register_code(data2);
+    } else {
+        unregister_code(data2);
+        unregister_code(data1);
+    }
+    return false;  // Skip all further processing of this key
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
 
         // macOS keys
-        case KC_MISSION_CONTROL:
-            if (record->event.pressed) {
-                host_consumer_send(0x29F);
-            } else {
-                host_consumer_send(0);
-            }
-            return false;  // Skip all further processing of this key
-        case KC_LAUNCHPAD:
-            if (record->event.pressed) {
-                host_consumer_send(0x2A0);
-            } else {
-                host_consumer_send(0);
-            }
-            return false;  // Skip all further processing of this key
+        case KC_MISSION_CONTROL: return __consumer_send(record, _AC_SHOW_ALL_WINDOWS);
+        case KC_LAUNCHPAD: return __consumer_send(record, _AC_SHOW_ALL_APPS);
+        case KC_LEFT_OPTION: return __code_1(record, KC_LOPT);
+        case KC_RIGHT_OPTION: return __code_1(record, KC_ROPT);
+        case KC_LEFT_COMMAND: return __code_1(record, KC_LCMD);
+        case KC_RIGHT_COMMAND: return __code_1(record, KC_RCMD);
+
+        // Windows keys
+        case KC_TASK_VIEW: return __code_2(record, KC_LWIN, KC_TAB);
+        case KC_FILE_EXPLORER: return __code_2(record, KC_LWIN, KC_E);
 
         // print firmware version
         case KC_VERSION:
