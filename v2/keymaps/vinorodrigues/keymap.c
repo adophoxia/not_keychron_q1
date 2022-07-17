4827e1e187d60f88b1329e93a658453d5a785635
Vino Rodrigues <github@m.vinorodrigues.com>
2022-07-17 21:28:44 +1000

    better layer indicator

--- v2/keymaps/vinorodrigues/keymap.c
+++ v2/keymaps/vinorodrigues/keymap.c
@@ -22,6 +22,8 @@ enum {
 #define KC_FN6 MO(_F6)
 #define KC_C_L LT(_F1, KC_CAPS)
 
+#define LED_LAYER_INDICATOR 0x10
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ansi_knob(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MPLY, KC_VOLD, KC_VOLU,        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
@@ -151,14 +153,16 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
     if (default_layer_state != lyr) {
         for (uint8_t i = led_min; i <= led_max; i++) {
-            switch(lyr) {
-                case 1: rgb_matrix_set_color(i, 0, 0, val); break;  // blue
-                case 2: rgb_matrix_set_color(i, 0, val, 0); break;  // green
-                case 3: rgb_matrix_set_color(i, val, 0, 0); break;  // red
-                case 4: rgb_matrix_set_color(i, val, 0, val); break;  // magenta
-                case 5: rgb_matrix_set_color(i, 0, val, val); break;  // cyan
-                case 6: rgb_matrix_set_color(i, val, val, 0); break;  // yellow
-                default: break;
+            if (g_led_config.flags[i] & LED_LAYER_INDICATOR) {
+                switch(lyr) {
+                    case 1: rgb_matrix_set_color(i, 0, 0, val); break;  // blue
+                    case 2: rgb_matrix_set_color(i, 0, val, 0); break;  // green
+                    case 3: rgb_matrix_set_color(i, val, 0, 0); break;  // red
+                    case 4: rgb_matrix_set_color(i, val, 0, val); break;  // magenta
+                    case 5: rgb_matrix_set_color(i, 0, val, val); break;  // cyan
+                    case 6: rgb_matrix_set_color(i, val, val, 0); break;  // yellow
+                    default: break;
+                }
             }
         }
     }
