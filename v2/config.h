90b6a236178874c1c514c369d25dbfa7bf2afcd6
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2022-10-29 13:08:13 +1100

    Update config.h

--- v2/config.h
+++ v2/config.h
@@ -20,7 +20,6 @@
     /* EFL/WL Driver Configuration */
     #define WEAR_LEVELING_LOGICAL_SIZE 2048                              // Number of bytes "exposed" to the rest of QMK and denotes the size of the usable EEPROM.
     #define WEAR_LEVELING_BACKING_SIZE (WEAR_LEVELING_LOGICAL_SIZE * 2)  // Number of bytes used by the wear-leveling algorithm for its underlying storage, and needs to be a multiple of the logical size.
-    #define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 2047
 #else
     #error "Please define the compile type in the `rules.mk` file"
 #endif
