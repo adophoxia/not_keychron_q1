46b2d38c09faa54f325dfe842a99d484186dc897
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2022-12-18 23:20:42 +1100

    data driven LED matrix

--- v2/ansi_knob/rules.mk
+++ v2/ansi_knob/rules.mk
@@ -1,6 +1,4 @@
 # Copyright 2022 Vino Rodrigues (@vinorodrigues)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# Build options
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = CKLED2001
+# File intentionaly left blank
