220aea0f5ed734aa2df41af083503296f736d7b1
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2023-05-09 17:09:52 +1000

    remove legacy code

--- v2/post_rules.mk
+++
@@ -1,7 +0,0 @@
-# Define your variant from the `make` command line `EEPROM=1`
-# or by adding `EEPROM yes` to the keymap rules.mk file.
-
-EEPROM ?= yes
-ifneq ($(strip $(EEPROM)), yes)
-	OPT_DEFS += -DEEPROM_ENABLE
-endif
